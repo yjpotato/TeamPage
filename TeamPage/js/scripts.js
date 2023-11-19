db68b0318efd70172ebfb945d79e97e7eb6dc69e
yjpotato <ponyojoa7467@gmail.com>
2023-11-19 20:51:09 +0900

    update yoojin

--- TeamPage/js/scripts.js
+++ TeamPage/js/scripts.js
@@ -1,6 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // 프로그래스 바 애니메이션 시작 함수
+    function startAnimation(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+                observer.unobserve(entry.target);
+            }
+        });
+    }
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+    };
+
+    const observer = new IntersectionObserver(startAnimation, options);
+
     // 헤더 불러오기
-    fetch('../includes/header.html')
+    fetch('/includes/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
@@ -10,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
     // 푸터 불러오기
-    fetch('../includes/footer.html')
+    fetch('/includes/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
@@ -19,14 +37,58 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log('푸터를 불러오는 데 실패했습니다: ', error);
         });
 
-    const progressBars = document.querySelectorAll('.progress-bar');
+    // 등장 오디오 자동 재생
+    document.body.addEventListener('click', startBackgroundAudio);
 
+    // MBTI 아이템에 마우스 오버 이벤트 리스너 등록
+    const mbtiItems = document.querySelectorAll('.mbti');
+    mbtiItems.forEach(item => {
+        item.addEventListener('mouseover', playAudio);
+    });
+
+    // 프로그래스 바 설정
+    const progressBars = document.querySelectorAll('.progress-bar');
     progressBars.forEach(bar => {
         observer.observe(bar);
         bar.parentElement.classList.add('visible');
     });
 
+    // 시작 함수
+    function start() {
+        // 프로그래스 바 설정
+        const progressBars = document.querySelectorAll('.progress-bar');
+        progressBars.forEach(bar => {
+            observer.observe(bar);
+            bar.parentElement.classList.add('visible');
+        });
+        // 등장 오디오 자동 재생
+        document.body.addEventListener('click', startBackgroundAudio);
+        // MBTI 아이템에 마우스 오버 이벤트 리스너 등록
+        const mbtiItems = document.querySelectorAll('.mbti');
+        mbtiItems.forEach(item => {
+            item.addEventListener('mouseover', playAudio);
+        });
+    }
+
+    // 등장 오디오 삽입
+    function startBackgroundAudio() {
+        const bgAudio = document.getElementById('bgAudio');
+        bgAudio.play().catch(error => {
+            console.error('등장 오디오를 재생하는 데 실패했습니다: ', error);
+        });
+    }
+
+    // 카드 오디오 삽입
+    function playAudio() {
+        const audio = document.getElementById('myAudio');
+        audio.play().catch(error => {
+            console.error('카드 오디오를 재생하는 데 실패했습니다: ', error);
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', start);
 });
+
 
 function checkEmailAvailability() {
     // 이메일 중복 확인을 위한 AJAX 요청을 수행하는 부분
@@ -64,7 +126,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 window.addEventListener('DOMContentLoaded', event => {
-    const content = "우리는 우주 최강 1조!!";
+    const content = "내가 선택한카드. 최강1조.";
     const text = document.querySelector(".text");
     let i = 0;
 
